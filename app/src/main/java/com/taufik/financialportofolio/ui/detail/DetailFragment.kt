61609ddf9d2c77cdfae0605e67ea713e2eec559b
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-10-29 07:46:07 +0700

    Implement detail transactions.

---
+++ app/src/main/java/com/taufik/financialportofolio/ui/detail/DetailFragment.kt
@@ -0,0 +1,81 @@
+package com.taufik.financialportofolio.ui.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.taufik.financialportofolio.R
+import com.taufik.financialportofolio.core.data.source.remote.response.Data
+import com.taufik.financialportofolio.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private val detailAdapter by lazy { DetailAdapter() }
+    private var listOfTrx: Data? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initToolbar()
+        initAdapter()
+        getBundleData()
+        setTransactionsData()
+    }
+
+    private fun initToolbar() {
+        binding.toolbarDetail.apply {
+            tvTitle.text = getString(R.string.txt_detail)
+            imgBack.visibility = View.VISIBLE
+            imgBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        binding.rvTransactions.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+            adapter = detailAdapter
+        }
+    }
+
+    private fun getBundleData() {
+        listOfTrx = arguments?.getParcelable(EXTRA_LIST_TRANSACTION)
+    }
+
+    private fun setTransactionsData() {
+        listOfTrx?.let { data ->
+            binding.tvLabelAndPercentage.text = getString(
+                R.string.txt_label_percentage,
+                data.label,
+                data.percentage,
+                "%"
+            )
+            detailAdapter.submitList(data.dataX)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        const val EXTRA_LIST_TRANSACTION = "com.taufik.financialportofolio.ui.detail.EXTRA_LIST_TRANSACTION"
+    }
+}
